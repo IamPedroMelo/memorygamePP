7c5ffa2a171e6ad81fffdb45088defe3a7e66262
Pedro melo <pedromelop@hotmail.com>
2018-06-01 00:24:58 +0100

    pontuaçao v1

---
+++ app/src/main/java/pt/ipg/memorygamepp/pontuacaoActivity.java
@@ -0,0 +1,21 @@
+package pt.ipg.memorygamepp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class pontuacaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pontuacao);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
