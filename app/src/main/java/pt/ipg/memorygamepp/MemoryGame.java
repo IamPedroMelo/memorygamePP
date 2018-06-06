97140b1cd7a89fd641bd8c1309170d929ed4bd73
IamPedroMelo <pedromelop@hotmail.com>
2018-06-06 21:00:49 +0100

    main java class import dos resourcess not working

--- app/src/main/java/pt/ipg/memorygamepp/MemoryGame.java
+++ app/src/main/java/pt/ipg/memorygamepp/MemoryGame.java
@@ -1,9 +1,17 @@
 package pt.ipg.memorygamepp;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.widget.AppCompatDrawableManager;
 import android.widget.Button;
+import android.widget.GridLayout;
 
-public class MemoryGame extends Button {
+import pt.ipg.memorygamepp.R;
+
+public class MemoryGame extends android.support.v7.widget.AppCompatButton {
 
     protected int row;
     protected int column;
@@ -12,12 +20,38 @@ public class MemoryGame extends Button {
     protected boolean isFlipped = false;
     protected boolean isMatched = false;
 
+    protected Drawable front;
+    protected Drawable back;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @SuppressLint("RestrictedApi")
     public MemoryGame (Context context , int r, int c , int frontImageDrawableId) {
         super(context);
 
         row = r;
         column = c;
         frontDrawableId = frontImageDrawableId;
+
+        front = AppCompatDrawableManager.get().getDrawable(context, frontImageDrawableId);
+        back = AppCompatDrawableManager.get().getDrawable(context, android.R.drawable.botao_question_mark);
+
+        setBackground(back);
+
+        GridLayout.LayoutParams tempParams = new GridLayout.LayoutParams(GridLayout.spec(r),GridLayout.spec(c));
+
+        tempParams.width = (int) getResources().getDisplayMetrics().density*50;
+        tempParams.height = (int) getResources().getDisplayMetrics().density*50;
+
+        setLayoutParams(tempParams);
+
     }
 
+    public boolean isMatched() {
+        return isMatched;
+    }
+
+    public void setMatched(boolean matched) {
+        isMatched = matched;
+    }
 }
