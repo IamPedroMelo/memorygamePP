82e5af4c2d0ba79203bd7abc5b42d57f16206004
IamPedroMelo <pedromelop@hotmail.com>
2018-06-12 22:08:40 +0100

    findviews das imagens

--- app/src/main/java/pt/ipg/memorygamepp/GameActivity.java
+++ app/src/main/java/pt/ipg/memorygamepp/GameActivity.java
@@ -16,12 +16,11 @@ import android.widget.GridLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
-
-   // private static final int BUTTONS_MARGIN = 100;
 
 
     private Button[][] button = new Button[4][4];
@@ -39,6 +38,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     //int clickedFirst, clickedSecond;
     //int cardNumber =1;
 
+    int playerScore = 0;
+
 
     @android.support.annotation.RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
@@ -48,46 +49,23 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        iv_01 = (ImageView) findViewById(R.id.iv_01);
+        iv_02 = (ImageView) findViewById(R.id.iv_02);
+        iv_03 = (ImageView) findViewById(R.id.iv_03);
+        iv_04 = (ImageView) findViewById(R.id.iv_04);
+        iv_05 = (ImageView) findViewById(R.id.iv_05);
+        iv_06 = (ImageView) findViewById(R.id.iv_06);
+        iv_07 = (ImageView) findViewById(R.id.iv_07);
+        iv_08 = (ImageView) findViewById(R.id.iv_08);
+        iv_09 = (ImageView) findViewById(R.id.iv_09);
+        iv_10 = (ImageView) findViewById(R.id.iv_10);
+        iv_11 = (ImageView) findViewById(R.id.iv_11);
+        iv_12 = (ImageView) findViewById(R.id.iv_12);
+        iv_13 = (ImageView) findViewById(R.id.iv_13);
+        iv_14 = (ImageView) findViewById(R.id.iv_14);
+        iv_15 = (ImageView) findViewById(R.id.iv_15);
+        iv_16 = (ImageView) findViewById(R.id.iv_16);
 
-
-        /*button[0][0] = (Button) findViewById(R.id.button00);
-        button[0][1] = (Button) findViewById(R.id.button01);
-        button[0][2] = (Button) findViewById(R.id.button02);
-        button[0][3] = (Button) findViewById(R.id.button03);
-        button[1][0] = (Button) findViewById(R.id.button10);
-        button[1][1] = (Button) findViewById(R.id.button11);
-        button[1][2] = (Button) findViewById(R.id.button12);
-        button[1][3] = (Button) findViewById(R.id.button13);
-        button[2][0] = (Button) findViewById(R.id.button20);
-        button[2][1] = (Button) findViewById(R.id.button21);
-        button[2][2] = (Button) findViewById(R.id.button22);
-        button[2][3] = (Button) findViewById(R.id.button23);
-        button[3][0] = (Button) findViewById(R.id.button30);
-        button[3][1] = (Button) findViewById(R.id.button31);
-        button[3][2] = (Button) findViewById(R.id.button32);
-        button[3][3] = (Button) findViewById(R.id.button33);
-    */
-
-
-
-       /* Display display = getWindowManager().getDefaultDisplay();
-
-        Point size = new Point();
-        display.getSize(size);
-
-        int width = size.x < size.y ? size.x : size.y;
-        width -= BUTTONS_MARGIN * 3;
-
-
-        for (int row = 0; row < 4; row++) {
-            for (int col = 0; col < 4; col++) {
-
-                button[row][col].setLayoutParams(new GridLayout.LayoutParams());
-
-                //button[row][col].setWidth(width/4);
-                //button[row][col].setHeight(width/4);
-            }
-        }*/
 
 
     }
