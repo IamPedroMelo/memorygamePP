3f1a22bd84cc664ac9ac21403c4b0b15cccfb312
IamPedroMelo <pedromelop@hotmail.com>
2018-06-12 20:54:20 +0100

    is flipped is matched

---
+++ app/src/main/java/pt/ipg/memorygamepp/Memory.java
@@ -0,0 +1,34 @@
+package pt.ipg.memorygamepp;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatDrawableManager;
+
+public class Memory {
+
+
+    private boolean isMatched = false;
+    private boolean isFlipped = false;
+    private Drawable back;
+
+    back = AppCompatDrawableManager.get().getDrawable(R.drawable.botao_question_mark);
+
+    setBackground(back);
+
+
+    public boolean isMatched() {
+        return isMatched;
+    }
+
+    public void setMatched(boolean matched) {
+        isMatched = matched;
+    }
+
+    public boolean isFlipped() {
+        return isFlipped;
+    }
+
+    public void setFlipped(boolean flipped) {
+        isFlipped = flipped;
+    }
+
+}
