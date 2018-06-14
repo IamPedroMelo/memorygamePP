9eb815465dbf536ec190fac9528109a51dfc0b66
IamPedroMelo <pedromelop@hotmail.com>
2018-06-14 22:38:55 +0100

    pontuação vfinal1

--- app/src/main/java/pt/ipg/memorygamepp/pontuacaoActivity.java
+++ app/src/main/java/pt/ipg/memorygamepp/pontuacaoActivity.java
@@ -1,5 +1,6 @@
 package pt.ipg.memorygamepp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,8 +20,11 @@ public class pontuacaoActivity extends AppCompatActivity {
     }
 
     public void VoltaMenu(View view) {
+        Intent intent = new Intent(this,menuActivity.class);
+        startActivity(intent);
     }
 
     public void Sair(View view) {
+        finish();
     }
 }
