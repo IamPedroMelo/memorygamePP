936fde5386cc7c2be762f3258d2a11f76835cfd8
IamPedroMelo <pedromelop@hotmail.com>
2018-06-18 22:43:50 +0100

    Tabela Users criada em SQL

--- app/src/main/java/pt/ipg/memorygamepp/DbTableUsers.java
+++ app/src/main/java/pt/ipg/memorygamepp/DbTableUsers.java
@@ -4,6 +4,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
 public class DbTableUsers implements BaseColumns {
+
+
+    public static final String TABLE_NAME = "Users";
+    public static final String FIELD_USERNAME = "Username";
+
     private final SQLiteDatabase db;
 
     public DbTableUsers(SQLiteDatabase db) {
@@ -11,6 +16,12 @@ public class DbTableUsers implements BaseColumns {
     }
 
     public void create() {
+        db.execSQL(
+                "CREATE TABLE " + TABLE_NAME + "(" + _ID +
+                        " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        FIELD_USERNAME + " TEXT NOT NULL" +
+                        ")"
 
+        );
     }
 }
