561e9d5162ce9e23c98f34291434c756141392a2
IamPedroMelo <pedromelop@hotmail.com>
2018-06-18 23:04:32 +0100

    HighScore e Users class created

---
+++ app/src/main/java/pt/ipg/memorygamepp/Users.java
@@ -0,0 +1,24 @@
+package pt.ipg.memorygamepp;
+
+public class Users {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String username;
+
+}
