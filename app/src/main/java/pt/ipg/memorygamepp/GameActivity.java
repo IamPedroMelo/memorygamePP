b12bf7b6d82cb1ca3bace103f772f7e27fe8ee5f
IamPedroMelo <pedromelop@hotmail.com>
2018-06-21 18:12:00 +0100

    GameActivity, completamente reformulado (falta delay)

--- app/src/main/java/pt/ipg/memorygamepp/GameActivity.java
+++ app/src/main/java/pt/ipg/memorygamepp/GameActivity.java
@@ -1,49 +1,28 @@
 package pt.ipg.memorygamepp;
 
-import android.annotation.TargetApi;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.RequiresApi;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
-import android.view.Display;
 import android.view.KeyEvent;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.GridLayout;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Random;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
-
-    private Button[][] button = new Button[4][4];
-
     TextView tv_01, tv_02;
 
-    //ImageView iv_01, iv_02, iv_03, iv_04, iv_05, iv_06, iv_07, iv_08, iv_09, iv_10, iv_11, iv_12, iv_13, iv_14, iv_15, iv_16;
     ImageView iv [][] = new ImageView[4][4];
 
-
-    //array das imagens
-    //Integer[] imgarray = {101, 102, 103, 104, 105, 106, 107, 108, 201, 202, 203, 204, 205, 206, 207, 208};
+    //matriz das imagens
     int[][] imagens = new int[][] {
         {101, 102, 103, 104},
         {105, 106, 107, 108},
@@ -55,12 +34,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     int imagem101, imagem102, imagem103, imagem104, imagem105, imagem106, imagem107, imagem108, imagem201, imagem202, imagem203, imagem204, imagem205, imagem206, imagem207, imagem208;
 
     int firstImage, secondImage;
-    int clickedFirst, clickedSecond;
     int cardNumber = 1;
     private String username;
-
-
-
 
     int playerScore = 0;
 
@@ -189,45 +164,16 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         // ver imagem selecionada e guardar temporariamente
         if(cardNumber == 1){
             firstImage = imagens[linha][coluna];
-            /*if(firstImage > 200){
-                firstImage = firstImage-100;
-            }*/
+
             cardNumber = 2;
-            clickedFirst = imagens[linha][coluna];
 
             imgv.setEnabled(false);
         } else if(cardNumber ==2){
             secondImage = imagens[linha][coluna];
-            /*if(secondImage > 200){
-                secondImage = secondImage-100;
-            }*/
+
             cardNumber = 1;
-            clickedSecond = imagens[linha][coluna];
 
-            /*for (int l = 0; linha < 4; linha++) {
-                for (int c = 0; coluna < 4; coluna++) {
-                    iv[linha][coluna].setEnabled(false);
-                }
-            }*/
-            iv[linha][coluna].setEnabled(false);
-            
-            /*iv_01.setEnabled(false);
-            iv_02.setEnabled(false);
-            iv_03.setEnabled(false);
-            iv_04.setEnabled(false);
-            iv_05.setEnabled(false);
-            iv_06.setEnabled(false);
-            iv_07.setEnabled(false);
-            iv_08.setEnabled(false);
-            iv_09.setEnabled(false);
-            iv_10.setEnabled(false);
-            iv_11.setEnabled(false);
-            iv_12.setEnabled(false);
-            iv_13.setEnabled(false);
-            iv_14.setEnabled(false);
-            iv_15.setEnabled(false);
-            iv_16.setEnabled(false);*/
-
+            imgv.setEnabled(false);
 
             Handler handler = new Handler();
             handler.postDelayed(new Runnable() {
@@ -236,7 +182,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                     calculate();
                 }
             },1000);
-
         }
     }
 
@@ -247,97 +192,15 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 for (int coluna = 0; coluna < 4; coluna++) {
                     int tag = (Integer) iv[linha][coluna].getTag();
                     int l= tag/10;
-                    int c= tag%0;
-                    if(clickedFirst == imagens[l][c]){
+                    int c= tag%10;
+                    if(firstImage == imagens[l][c]){
                         iv[linha][coluna].setVisibility(View.INVISIBLE);
                     }
                 }
             }
-            
-            /*if (clickedFirst == 0) {
-                iv_01.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 1) {
-                iv_02.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 2) {
-                iv_03.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 3) {
-                iv_04.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 4) {
-                iv_05.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 5) {
-                iv_06.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 6) {
-                iv_07.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 7) {
-                iv_08.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 8) {
-                iv_09.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 9) {
-                iv_10.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 10) {
-                iv_11.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 11) {
-                iv_12.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 12) {
-                iv_13.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 13) {
-                iv_14.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 14) {
-                iv_15.setVisibility(View.INVISIBLE);
-            } else if (clickedFirst == 15) {
-                iv_16.setVisibility(View.INVISIBLE);
-            }*/
-
-            for (int linha = 0; linha < 4; linha++) {
-                for (int coluna = 0; coluna < 4; coluna++) {
-                    int tag = (Integer) iv[linha][coluna].getTag();
-                    int l= tag/10;
-                    int c= tag%0;
-                    if(clickedSecond == imagens[l][c]){
-                        iv[linha][coluna].setVisibility(View.INVISIBLE);
-                    }
-                }
-            }
-
-            /*if (clickedSecond == 0) {
-                iv_01.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 1) {
-                iv_02.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 2) {
-                iv_03.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 3) {
-                iv_04.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 4) {
-                iv_05.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 5) {
-                iv_06.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 6) {
-                iv_07.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 7) {
-                iv_08.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 8) {
-                iv_09.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 9) {
-                iv_10.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 10) {
-                iv_11.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 11) {
-                iv_12.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 12) {
-                iv_13.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 13) {
-                iv_14.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 14) {
-                iv_15.setVisibility(View.INVISIBLE);
-            } else if (clickedSecond == 15) {
-                iv_16.setVisibility(View.INVISIBLE);
-            }*/
 
             playerScore = playerScore +10;
             tv_01.setText(playerScore+" POINTS");
-
-
-
         } else {
 
             if(playerScore>=5){
@@ -345,28 +208,12 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 tv_01.setText(playerScore+" POINTS");
             }
 
+
             for (int linha = 0; linha < 4; linha++) {
                 for (int coluna = 0; coluna < 4; coluna++) {
                     iv[linha][coluna].setImageResource(R.drawable.image_question_mark);
                 }
             }
-
-            /*iv_01.setImageResource(R.drawable.image_question_mark);
-            iv_02.setImageResource(R.drawable.image_question_mark);
-            iv_03.setImageResource(R.drawable.image_question_mark);
-            iv_04.setImageResource(R.drawable.image_question_mark);
-            iv_05.setImageResource(R.drawable.image_question_mark);
-            iv_06.setImageResource(R.drawable.image_question_mark);
-            iv_07.setImageResource(R.drawable.image_question_mark);
-            iv_08.setImageResource(R.drawable.image_question_mark);
-            iv_09.setImageResource(R.drawable.image_question_mark);
-            iv_10.setImageResource(R.drawable.image_question_mark);
-            iv_11.setImageResource(R.drawable.image_question_mark);
-            iv_12.setImageResource(R.drawable.image_question_mark);
-            iv_13.setImageResource(R.drawable.image_question_mark);
-            iv_14.setImageResource(R.drawable.image_question_mark);
-            iv_15.setImageResource(R.drawable.image_question_mark);
-            iv_16.setImageResource(R.drawable.image_question_mark);*/
 
         }
 
@@ -375,24 +222,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 iv[linha][coluna].setEnabled(true);
             }
         }
-
-        /*iv_01.setEnabled(true);
-        iv_02.setEnabled(true);
-        iv_03.setEnabled(true);
-        iv_04.setEnabled(true);
-        iv_05.setEnabled(true);
-        iv_06.setEnabled(true);
-        iv_07.setEnabled(true);
-        iv_08.setEnabled(true);
-        iv_09.setEnabled(true);
-        iv_10.setEnabled(true);
-        iv_11.setEnabled(true);
-        iv_12.setEnabled(true);
-        iv_13.setEnabled(true);
-        iv_14.setEnabled(true);
-        iv_15.setEnabled(true);
-        iv_16.setEnabled(true);*/
-
         checkEnd();
     }
     
@@ -408,23 +237,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     }
 
         private void checkEnd(){
-            if (checkVisible()==true
-                    /*iv_01.getVisibility() == View.INVISIBLE &&
-                    iv_02.getVisibility() == View.INVISIBLE &&
-                    iv_03.getVisibility() == View.INVISIBLE &&
-                    iv_04.getVisibility() == View.INVISIBLE &&
-                    iv_05.getVisibility() == View.INVISIBLE &&
-                    iv_06.getVisibility() == View.INVISIBLE &&
-                    iv_07.getVisibility() == View.INVISIBLE &&
-                    iv_08.getVisibility() == View.INVISIBLE &&
-                    iv_09.getVisibility() == View.INVISIBLE &&
-                    iv_10.getVisibility() == View.INVISIBLE &&
-                    iv_11.getVisibility() == View.INVISIBLE &&
-                    iv_12.getVisibility() == View.INVISIBLE &&
-                    iv_13.getVisibility() == View.INVISIBLE &&
-                    iv_14.getVisibility() == View.INVISIBLE &&
-                    iv_15.getVisibility() == View.INVISIBLE &&
-                    iv_16.getVisibility() == View.INVISIBLE*/) {
+            if (checkVisible()==true) {
 
                 AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(GameActivity.this);
                 alertDialogBuilder
@@ -460,15 +273,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             imagem106 = R.drawable.image106;
             imagem107 = R.drawable.image107;
             imagem108 = R.drawable.image108;
-            imagem201 = R.drawable.image201;
-            imagem202 = R.drawable.image202;
-            imagem203 = R.drawable.image203;
-            imagem204 = R.drawable.image204;
-            imagem205 = R.drawable.image205;
-            imagem206 = R.drawable.image206;
-            imagem207 = R.drawable.image207;
-            imagem208 = R.drawable.image208;
-
         }
 
 
@@ -495,7 +299,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         });
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
-        //alertDialog.setCanceledOnTouchOutside(false);
+        alertDialog.setCanceledOnTouchOutside(false);
 
     }
 }
