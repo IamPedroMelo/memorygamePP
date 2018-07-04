b9914797f74be9121bc49dc3e34d8bcd2c9f448e
IamPedroMelo <pedromelop@hotmail.com>
2018-07-05 00:16:54 +0100

    Atualizações
    Atualizações

--- app/src/main/java/pt/ipg/memorygamepp/menuActivity.java
+++ app/src/main/java/pt/ipg/memorygamepp/menuActivity.java
@@ -2,15 +2,10 @@ package pt.ipg.memorygamepp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.GridLayout;
+
 
 public class menuActivity extends AppCompatActivity {
 
@@ -20,8 +15,6 @@ public class menuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_activity);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
         buttonjogo = (Button) findViewById(R.id.buttonnovojogo);
 
@@ -32,40 +25,7 @@ public class menuActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
-
-
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
-
-    /*public void MudaActivityGame(View view) {
-
-        Intent intent = new Intent(this,GameActivity.class);
-        startActivity(intent);
-
-    }*/
 
     @Override
     public void onBackPressed() {
